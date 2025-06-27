9f45c442c32037406cdc250ba2f9fb92191b017a
Dixie Philamerah Atay <dixie.atay@gmail.com>
2025-06-27 17:21:36 +0800

    Make changes to db clients.

--- db/dbclient-pg.ts
+++ db/dbclient-pg.ts
@@ -1,10 +1,9 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/postgres-js";
 import { sql } from "drizzle-orm";
+import postgres from "npm:postgres";
 import * as schema from "./schema/all_schema.ts";
 
-
-export const client = neon(Deno.env.get("DATABASE_URL")!);
+export const client = postgres(Deno.env.get("DATABASE_URL")!);
 export const db = drizzle({ client, schema });
 
 export function lower(email: AnyPgColumn): SQL {
